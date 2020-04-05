8052d4d1703125fb53dfa0461c4cd354402855e3
ripter <ripter001@gmail.com>
2020-04-05 16:36:28 -0700

    match-mesh component now works. It sets a target's geometry, position, and rotation to a copy from an existing Mesh.

--- demos/inky-panel/src/components/match-mesh.js
+++ demos/inky-panel/src/components/match-mesh.js
@@ -3,8 +3,8 @@
  */
 AFRAME.registerComponent('match-mesh', {
   schema: {
-    src: { type: 'selector' },
-    name: { type: 'string' },
+    target: { type: 'selector' },
+    meshName: { type: 'string' },
   },
 
   /**
@@ -13,8 +13,7 @@ AFRAME.registerComponent('match-mesh', {
    * Components can use this to set initial state.
    */
   init() {
-    // Listen for add/remove of key objects mesh and armature
-    // this.el.addEventListener('object3dset', this);
+    this.el.addEventListener('model-loaded', this);
   },
 
   /**
@@ -25,58 +24,21 @@ AFRAME.registerComponent('match-mesh', {
    * @param {object} prevData - Previous attributes of the component.
    */
   update(prevData) {
-    const { src, name } = this.data;
+    const { target, meshName } = this.data;
+    if (!target || !meshName) { return; }
+    const mesh = this.el.object3D.getObjectByName(meshName);
+    if (!mesh) { return; }
+    const targetMesh = target.getObject3D('mesh');
+    if (!targetMesh) { return; }
 
-    if (src) {
-      // does it have a mesh yet?
-      const scene = src.getObject3D('mesh');
-      if (scene) {
-        this.updateToMesh();
-      }
-      else {
-        src.addEventListener('object3dset', () => this.updateToMesh());
-      }
-    }
-    // console.group('update');
-    // console.log('data', this.data);
-    // console.log('prevData', prevData);
-    // console.log('src', src);
-    // console.log('mesh', mesh);
-    // console.groupEnd();
+    // Replace the target geometry with the the mesh's geometry
+    targetMesh.geometry.copy(mesh.geometry);
+    // Match the position & rotation
+    targetMesh.position.copy(mesh.position);
+    targetMesh.quaternion.copy(mesh.quaternion);
 
-
-  },
-
-  /**
-   * Tick handler.
-   * Called on each tick of the scene render loop.
-   * Affected by play and pause.
-   *
-   * @param {number} time - Scene tick time.
-   * @param {number} timeDelta - Difference in current render time and previous render time.
-   */
-  tick(time, timeDelta) {
-  },
-
-  /**
-   * Called to start any dynamic behavior (e.g., animation, AI, events, physics).
-   */
-  play() {
-  },
-
-  /**
-   * Called to stop any dynamic behavior (e.g., animation, AI, events, physics).
-   */
-  pause() {
-  },
-
-
-  /**
-   * Remove handler. Similar to detachedCallback.
-   * Called whenever component is removed from the entity (i.e., removeAttribute).
-   * Components can use this to reset behavior on the entity.
-   */
-  remove() {
+    // Turn off the mesh we are copying.
+    mesh.visible = false;
   },
 
   /**
@@ -86,43 +48,6 @@ AFRAME.registerComponent('match-mesh', {
    * @return {undefined}
    */
   handleEvent(event) {
-    switch (event.type) {
-      case 'model-loaded':
-        console.log('event', event);
-        this.updateToMesh();
-        break;
-      default:
-        console.warn(`Unhandled event type: ${event.type}`, event); // eslint-disable-line
-    }
-  },
-
-
-  updateToMesh() {
-    const { src, name } = this.data;
-    if (!src || !name) { return; }
-    const scene = src.getObject3D('mesh');
-    if (!scene) { return; }
-    const mesh = scene.getObjectByName(name);
-    const pos = new THREE.Vector3();
-
-    mesh.getWorldPosition(pos);
-    pos.y += 0.5;
-    mesh.position.copy(pos);
-    // this.el.object3D.position.copy(pos);
-    // pos.copy(mesh.position);
-    this.el.object3D.applyMatrix(mesh.matrixWorld);
-
-    console.group('updateToMesh');
-    console.log('src', src);
-    console.log('mesh', mesh);
-    console.log('pos', pos);
-    console.groupEnd();
-
-
-    // this.el.object3D.position.copy(mesh.position);
-    // mesh.getWorldPosition(this.el.object3D.position);
-    // mesh.matrixWorld.copy(this.el.object3D.matrixWorld);
-    // debugger;
-    // this.el.object3D.applyMatrix(mesh.matrixWorld);
+    this.update();
   },
 });
